7f5fe77df58ff0812845c1082afa477bdc0e35ff
Mxbz33 <waykiki.aloha@gmail.com>
2022-06-02 12:56:16 -0500

    1st commit

---
+++ basicOperators.py
@@ -0,0 +1,14 @@
+nombre = "Maximiliano B" # String - palabra, frase, texto
+print(nombre)
+nombre = "El Rey"
+print(nombre)
+
+edad = 22 # Entero - Sin punto decimal
+estatura = 1.75 # Float - Con punto decimal
+print(edad + estatura) # Suma
+print(edad - estatura) # Resta
+print(edad * estatura) # Multiplicación
+print(edad / estatura) # División
+print(edad % estatura) # Modulo 
+print(edad // estatura) # División enterea
+print(edad ** 2) # Potencia
---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print("Que pedo gente")
+
+a = int(input("digite su edad "))
+
+if a < 18 :
+    print("Tas muy morro, pa su casa")
+elif a<0 :
+    print("No existes mijo")
+elif a>100 :
+    print("Aun vives?")
+else :
+    print("Tome una chela y una ruca")
